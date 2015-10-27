40e99d8871e2797b16d75f88c24e9e942ef86191
TawaSystems <tawasystems@gmail.com>
2015-10-27 22:19:42 +0300

    Произведен рефакторин классов Autorization, Bribe

--- BeloteServer/BeloteServer/BeloteServer/Bribe.cs
+++ BeloteServer/BeloteServer/BeloteServer/Bribe.cs
@@ -9,68 +9,43 @@ namespace BeloteServer
     // Класс, представляющий каждую конкретную взятку
     class Bribe
     {
-        public Card c1, c2, c3, c4;
         public Bribe()
         {
-            c1 = null;
-            c2 = null;
-            c3 = null;
-            c4 = null;
+            Player1 = null;
+            Player2 = null;
+            Player3 = null;
+            Player4 = null;
         }
 
         public Card Player1
         {
-            get
-            {
-                return c1;
-            }
-            set
-            {
-                c1 = value;
-            }
+            get;
+            set;
         }
 
         public Card Player2
         {
-            get
-            {
-                return c2;
-            }
-            set
-            {
-                c2 = value;
-            }
+            get;
+            set;
         }
 
         public Card Player3
         {
-            get
-            {
-                return c3;
-            }
-            set
-            {
-                c3 = value;
-            }
+            get;
+            set;
         }
 
         public Card Player4
         {
-            get
-            {
-                return c4;
-            }
-            set
-            {
-                c4 = value;
-            }
+            get;
+            set;
         }
 
         public bool IsEnded
         {
             get
             {
-                return ((c1 != null) && (c2 != null) && (c3 != null) && (c4 != null));
+                return ((Player1 != null) && (Player2 != null) && (Player3 != null) && (Player4 != null));
             }
         }
     }
