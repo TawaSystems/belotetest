2683a9fc86c80d565c0956d1ceaf338b4685b719
TawaSystems <tawasystems@gmail.com>
2015-10-27 20:08:00 +0300

    Добавлен класс DistributionsList и его основные свойства

---
+++ BeloteServer/BeloteServer/BeloteServer/DistributionsList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeloteServer
+{
+    class DistributionsList
+    {
+        private List<Distribution> list;
+
+        public DistributionsList()
+        {
+            list = new List<Distribution>();
+        }
+
+        public void AddNew()
+        {
+            list.Add(new Distribution());
+        }
+
+        public Distribution Current
+        {
+            get
+            {
+                if (list.Count == 0)
+                    return null;
+                return list[list.Count - 1];
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+
+        public int ScoresTeam1
+        {
+            get
+            {
+                int Res = 0;
+                foreach (Distribution d in list)
+                {
+                    if (d.Status == DistributionStatus.D_ENDED)
+                        Res += d.ScoresTeam1;
+                }
+                return Res;
+            }
+        }
+
+        public int ScoresTeam2
+        {
+            get
+            {
+                int Res = 0;
+                foreach (Distribution d in list)
+                {
+                    if (d.Status == DistributionStatus.D_ENDED)
+                        Res += d.ScoresTeam2;
+                }
+                return Res;
+            }
+        }
+    }
+}
