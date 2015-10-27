d7f5f013cd4e625c5618d30fbb8b8100238b2487
TawaSystems <tawasystems@gmail.com>
2015-10-27 22:18:10 +0300

    Произведен рефакторинг класса Player

--- BeloteServer/BeloteServer/BeloteServer/Player.cs
+++ BeloteServer/BeloteServer/BeloteServer/Player.cs
@@ -9,13 +9,6 @@ namespace BeloteServer
 {
     class Player
     {
-        // Статистика игрока
-        private Statistics statistics;
-        // Ссылка на основной игровой объект
-        private Game game;
-        // Ссылка на профиль
-        private Profile profile;
-
         public Player(Game game) : this (game, -1)
         {
         }
@@ -23,9 +16,9 @@ namespace BeloteServer
         // Конструктор - создание по ID и основному игровому объекту
         public Player(Game game, int Id)
         {
-            this.game = game;
-            statistics = new Statistics();
-            profile = new Profile();
+            this.Game = game;
+            Statistics = new Statistics();
+            Profile = new Profile();
             if (Id >= 0)
             {
                 ReadPlayerFromDataBase("ID", Id);
@@ -44,75 +37,75 @@ namespace BeloteServer
             int value;
             if (Int32.TryParse(playerStatistics[2][0], out value))
             {
-                this.statistics.GamesTotal = value;
+                this.Statistics.GamesTotal = value;
             }
             if (Int32.TryParse(playerStatistics[3][0], out value))
             {
-                this.statistics.WinsCount = value;
+                this.Statistics.WinsCount = value;
             }
             if (Int32.TryParse(playerStatistics[4][0], out value))
             {
-                this.statistics.LosesCount = value;
+                this.Statistics.LosesCount = value;
             }
             if (Int32.TryParse(playerStatistics[5][0], out value))
             {
-                this.statistics.XP = value;
+                this.Statistics.XP = value;
             }
             if (Int32.TryParse(playerStatistics[6][0], out value))
             {
-                this.statistics.Level = value;
+                this.Statistics.Level = value;
             }
             if (Int32.TryParse(playerStatistics[7][0], out value))
             {
-                this.statistics.LastSeriesResult = value;
+                this.Statistics.LastSeriesResult = value;
             }
             if (Int32.TryParse(playerStatistics[8][0], out value))
             {
-                this.statistics.AbandonedGames = value;
+                this.Statistics.AbandonedGames = value;
             }
             if (Int32.TryParse(playerStatistics[9][0], out value))
             {
-                this.statistics.GamesNotAllowed = value;
+                this.Statistics.GamesNotAllowed = value;
             }
             if (Int32.TryParse(playerStatistics[10][0], out value))
             {
-                this.statistics.TablesCreated = value;
+                this.Statistics.TablesCreated = value;
             }
             if (Int32.TryParse(playerStatistics[11][0], out value))
             {
-                this.statistics.TablesCreatedNotPlayed = value;
+                this.Statistics.TablesCreatedNotPlayed = value;
             }
             if (Int32.TryParse(playerStatistics[12][0], out value))
             {
-                this.statistics.DistributionsTotal = value;
+                this.Statistics.DistributionsTotal = value;
             }
             if (Int32.TryParse(playerStatistics[13][0], out value))
             {
-                this.statistics.DistributionsWithTrump = value;
+                this.Statistics.DistributionsWithTrump = value;
             }
             if (Int32.TryParse(playerStatistics[14][0], out value))
             {
-                this.statistics.DistributionsAnnouncedWin = value;
+                this.Statistics.DistributionsAnnouncedWin = value;
             }
             if (Int32.TryParse(playerStatistics[15][0], out value))
             {
-                this.statistics.DistributionsWins = value;
+                this.Statistics.DistributionsWins = value;
             }
             if (Int32.TryParse(playerStatistics[16][0], out value))
             {
-                this.statistics.ContraAnnounced = value;
+                this.Statistics.ContraAnnounced = value;
             }
             if (Int32.TryParse(playerStatistics[17][0], out value))
             {
-                this.statistics.ContraJustified = value;
+                this.Statistics.ContraJustified = value;
             }
             if (Int32.TryParse(playerStatistics[18][0], out value))
             {
-                this.statistics.CapotAnnounced = value;
+                this.Statistics.CapotAnnounced = value;
             }
             if (Int32.TryParse(playerStatistics[19][0], out value))
             {
-                this.statistics.CapotJustified = value;
+                this.Statistics.CapotJustified = value;
             }
 #if DEBUG
             Debug.Unindent();
@@ -132,44 +125,44 @@ namespace BeloteServer
             DateTime d;
             if (Int32.TryParse(playerData[0][0], out value))
             {
-                this.profile.Id = value;
+                this.Profile.Id = value;
             }
-            this.profile.Nickname = playerData[1][0];
-            this.profile.Name = playerData[2][0];
-            this.profile.Surname = playerData[3][0];
-            this.profile.Password = playerData[4][0];
-            this.profile.Email = playerData[5][0];
-            this.profile.Phone = playerData[6][0];
-            this.profile.VK = playerData[7][0];
-            this.profile.FB = playerData[8][0];
-            this.profile.OK = playerData[9][0];
-            this.profile.Country = playerData[10][0];
-            this.profile.Address = playerData[11][0];
-            this.profile.ZipCode = playerData[12][0];
-            this.profile.Language = playerData[13][0];
+            this.Profile.Nickname = playerData[1][0];
+            this.Profile.Name = playerData[2][0];
+            this.Profile.Surname = playerData[3][0];
+            this.Profile.Password = playerData[4][0];
+            this.Profile.Email = playerData[5][0];
+            this.Profile.Phone = playerData[6][0];
+            this.Profile.VK = playerData[7][0];
+            this.Profile.FB = playerData[8][0];
+            this.Profile.OK = playerData[9][0];
+            this.Profile.Country = playerData[10][0];
+            this.Profile.Address = playerData[11][0];
+            this.Profile.ZipCode = playerData[12][0];
+            this.Profile.Language = playerData[13][0];
             if (Int32.TryParse(playerData[14][0], out value))
             {
-                this.profile.Sex = (value > 0);
+                this.Profile.Sex = (value > 0);
             }
-            this.profile.TimeZone = playerData[15][0];
+            this.Profile.TimeZone = playerData[15][0];
             if (DateTime.TryParse(playerData[16][0], out d))
-                this.profile.BirtDate = d;
-            this.profile.AvatarFile = playerData[17][0];
+                this.Profile.BirtDate = d;
+            this.Profile.AvatarFile = playerData[17][0];
             if (DateTime.TryParse(playerData[18][0], out d))
             {
-                this.profile.VIPExperies = d;
+                this.Profile.VIPExperies = d;
             }
             if (Int32.TryParse(playerData[19][0], out value))
             {
-                this.profile.USD = value;
+                this.Profile.USD = value;
             }
             if (Int32.TryParse(playerData[20][0], out value))
             {
-                this.profile.BUSD = value;
+                this.Profile.BUSD = value;
             }
             if (Int32.TryParse(playerData[21][0], out value))
             {
-                this.profile.Chips = value;
+                this.Profile.Chips = value;
             }
             ReadStatisticsFromaDataBase(this.Profile.Id);
 #if DEBUG
@@ -177,28 +170,25 @@ namespace BeloteServer
 #endif
         }
 
+        // Ссылка на основной игровой объект
         public Game Game
         {
-            get
-            {
-                return game;
-            }
+            get;
+            private set;
         }
 
+        // Статистика игрока
         public Statistics Statistics
         {
-            get
-            {
-                return statistics;
-            }
+            get;
+            private set;
         }
 
+        // Ссылка на профиль
         public Profile Profile
         {
-            get
-            {
-                return profile;
-            }
+            get;
+            private set;
         }
         
     }
