a8a5e65e93ce6e128e4a4c28f2d5d3a74102ff39
TawaSystems <thandle@MBP-Valerij.Dlink>
2015-12-13 15:36:30 +0300

    Пробный кормит с мака, добавлен комментарий

--- BeloteServer/BeloteServer/BeloteServer/Autorization.cs
+++ BeloteServer/BeloteServer/BeloteServer/Autorization.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +7,7 @@ using System.Diagnostics;
 
 namespace BeloteServer
 {
+    // Класс, реализующий авторизацию
     class Autorization
     {
         private Game game;
