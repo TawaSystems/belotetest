fda35eafff2f4e75d4197debed17873b07b8c98c
TawaSystems <thandle@MBP-Valerij.Dlink>
2015-12-13 18:20:07 +0300

    Добавлен SplashScreen

---
+++ UnityBeloteClient/Assets/Scenes/SplashScreenScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenScript : MonoBehaviour {
+
+	public Texture2D tex;
+	private Rect texRect;
+
+	// Use this for initialization
+	void Start () {
+		texRect = new Rect(0, 0, Screen.width, Screen.height);
+
+		Debug.Log (tex);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI()
+	{
+		GUI.DrawTexture (texRect, tex);
+	}
+}
